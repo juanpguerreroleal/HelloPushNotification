b17b6f7b92f297854da7b1b5e8402ee768059224
Juan Guerrero <juanpabloguleal@gmail.com>
2020-08-18 13:18:43 -0500

    First Commit

---
+++ HelloPushNotification/HelloPushNotification/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using HelloPushNotification.ViewModels;
+using HelloPushNotification.Views;
+using Xamarin.Forms;
+
+namespace HelloPushNotification
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ HelloPushNotification/HelloPushNotification/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using HelloPushNotification.ViewModels;
+
+namespace HelloPushNotification.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
