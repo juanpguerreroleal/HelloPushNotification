b38180a595967a62e11c411f4a717611f53f7e96
Juan Guerrero <juanpabloguleal@gmail.com>
2020-08-19 13:10:53 -0500

    AboutPage updated

--- HelloPushNotification/HelloPushNotification/ViewModels/AboutViewModel.cs
+++ HelloPushNotification/HelloPushNotification/ViewModels/AboutViewModel.cs
@@ -10,7 +10,7 @@ namespace HelloPushNotification.ViewModels
         public AboutViewModel()
         {
             Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamain-quickstart"));
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://github.com/juanpguerreroleal"));
         }
 
         public ICommand OpenWebCommand { get; }
